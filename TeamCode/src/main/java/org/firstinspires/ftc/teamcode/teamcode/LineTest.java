85759632293eb22945e238b85c589ce20deb0452
ruchialavilli <ruchi.alavilli@gmail.com>
2025-01-27 21:57:18 -0800

    first code changes, getting gamepad set up!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/LineTest.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.teamcode;//package org.firstinspires.ftc.teamcode;
+//
+//import com.acmerobotics.roadrunner.geometry.Pose2d;
+//import com.acmerobotics.roadrunner.geometry.Vector2d;
+//import com.acmerobotics.roadrunner.trajectory.Trajectory;
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//
+//import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+//
+//@Autonomous(name="LineTest", group="AutoOpModes")
+//public class LineTest extends BaseAutoVisionOpMode {
+////testing our heading when issues arise
+//    public void runOpMode() throws InterruptedException {
+//
+//
+//            SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+//
+//            robot.arm = new DejaVuArm();
+//            robot.arm.init(hardwareMap, true);
+//
+//
+//            Pose2d startPose = new Pose2d(63, 32, Math.toRadians(0));
+//            drive.setPoseEstimate(startPose);
+////            robot.arm.axon_right.setPosition(SERVO_DOWN);
+////            robot.arm.hook_right.setPosition(SERVO_UNLIFT);//hold both pixels
+////            robot.arm.hook_left.setPosition(SERVO_LIFTED-0.33);
+//            telemetry.addLine("Robot ready for run");
+//            telemetry.update();
+////
+//            Trajectory traj0 = drive.trajectoryBuilder(startPose.plus(new Pose2d(0,0,Math.toRadians(0))))
+//                    .lineTo(new Vector2d(23, 32))
+//                    .build();
+//
+//            waitForStart();
+//            if(isStopRequested()) return;
+////
+////            robot.arm.moveArmToLevel(3);
+////            sleep(1000);
+////            robot.arm.axon_right.setPosition(SERVO_UP);
+////            sleep(1000);
+////            robot.arm.moveArmToLevel(2);
+////            sleep(1000);
+////            robot.arm.hook_right.setPosition(SERVO_LIFTED);
+////            sleep(500);
+////            robot.arm.moveArmToLevel(3);
+////            sleep(1000);
+////            robot.arm.axon_right.setPosition(SERVO_DOWN);
+////            sleep(500);
+////            robot.arm.moveArmToLevel(0);
+////            sleep(1500);
+////
+//            drive.followTrajectory(traj0);
+//
+//
+//
+//
+//
+//
+//
+//    }
+//}
+//
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/newopmode.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class newopmode extends LinearOpMode {
+
+    public final float threshholdConst = .1f;
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("leftFront");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("leftBack");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("rightFront");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("rightBack");
+        DcMotor leftArmMotor = hardwareMap.dcMotor.get("leftArmMotor");
+        DcMotor intakeMot = hardwareMap.dcMotor.get("intakeMotor");
+        DcMotor rightarmmotor = hardwareMap.dcMotor.get("rightArmMotor");
+
+
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+
+
+
+            float slide = gamepad2.left_trigger;
+            if (gamepad2.left_trigger < threshholdConst) slide = 0;
+            if (gamepad2.left_trigger > 1 - threshholdConst) slide = 1;
+            leftArmMotor.setPower(slide*0.5);
+            rightarmmotor.setPower(-slide*0.5);
+
+        }
+    }
+}
